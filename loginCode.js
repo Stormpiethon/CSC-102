f61b13f7997826c1aa90333b443b187d44b950b9
Alexander Brittain <105312701+Stormpiethon@users.noreply.github.com>
2022-06-07 11:22:12 -0700

    Add files via upload

---
+++ loginCode.js
@@ -0,0 +1,21 @@
+function checkCreds(){
+    console.log("checkCreds() started");
+    //create needed variables
+    var firstName;
+    var lastName;
+    var badgeNumb;
+    var fullName;
+
+    //move form data into variables
+
+
+    //add firstName, " ", lastName into fullName
+
+
+    //input validation
+
+
+    //if incorrect, present error in "loginStaus" div, else go to Alexander's Space Out Program
+
+    
+} 
